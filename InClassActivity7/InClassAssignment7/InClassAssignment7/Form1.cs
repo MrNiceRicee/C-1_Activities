3ae0f17f0a29759545d86b60f4f9b832392450f9
Joshua Santos <joshcsan@gmail.com>
2020-03-13 07:01:02 -0700

    initialize

--- InClassActivity7/InClassAssignment7/InClassAssignment7/Form1.cs
+++ InClassActivity7/InClassAssignment7/InClassAssignment7/Form1.cs
@@ -12,6 +12,8 @@ namespace InClassAssignment7
 {
     public partial class Form1 : Form
     {
+        private Random random = new Random();
+
 
         public int TwoIntSum(int number1, int number2)
         {
@@ -26,15 +28,66 @@ namespace InClassAssignment7
 
         public int TwoRandomInts()
         {
-            Random r = new Random();
-
-            return (+);
+            int xx1 = random.Next(1000);    
+            int xx2 = random.Next(1000);
+            return (xx1+xx2);
         }
 
+        public Boolean TripleDivisibleByThree(int number1, int number2, int number3)
+        {
+            if ((number1+number2+number3)%3==0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string ShorterOfTwoStrings(String word1, String word2)
+        {
+            var words = new String[] {word1, word2 };
+            return (words.OrderByDescending(s=>s.Length).Last());
+        }
+
+        public double BiggestInDoubleArray(double[] DoubleArray)
+        {
+            //var biggest = DoubleArray.Max(x=>x);
+            //return biggest;
+            return (DoubleArray.Max(x=>x));
+        }
+
+        public double[] GenerateFiftyNumbers()
+        {
+            double[] fifty = new double[50];
+            for (int i = 0; i < 50; i++)        //50 loops
+            {
+                fifty[i] = random.NextDouble() * random.Next(1000);
+            }
+            return fifty;
+        }
+
+        public Boolean SameBool(Boolean first, Boolean second)
+        {
+            if (first.Equals(second))
+            {
+                return true;
+            }else
+            {
+                return false;
+            }
+        }
+
+        public double IntTimesDouble(int NumberInt, double NumberDouble)
+        {
+            return (NumberInt*NumberDouble);
+        }
 
         public Form1()
         {
             InitializeComponent();
+            Console.WriteLine(IntTimesDouble(8,10.5));
         }
     }
 }
